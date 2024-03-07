998d8eb5ed6826c38bf0b8e5f742c98dc42b75a2
ChromaChroma <jona.beer@gmail.com>
2024-03-07 13:37:56 +0100

    Setup example api

--- server/app/Api/CodeProblem.hs
+++ server/app/Api/CodeProblem.hs
@@ -1,18 +1,56 @@
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PolyKinds         #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE TypeOperators     #-}
 module Api.CodeProblem where
 
--- import Data.Text
--- import Data.Time (UTCTime)
--- import Servant.API
+import Data.Aeson
+import Data.Proxy
+import Data.Text 
+import Data.Time (UTCTime (..), fromGregorian, secondsToDiffTime)
+import Data.Typeable (Typeable)
+import GHC.Generics
 
--- -- type UserAPI = "users" :> QueryParam "sortby" SortBy :> Get '[JSON] [User]
+import Network.Wai.Handler.Warp
+import Servant
+import Servant.Server.Generic ()
+import Servant.API.Generic ((:-))
+
+--API
+
+userApi :: Proxy UserAPI
+userApi = Proxy
+
+type UserAPI = 
+  "users" :> Get '[JSON] [User] 
+    -- | GET /hello/:name?capital={true, false}  returns a Greet as JSON
+  :<|> "users" :> Capture "name" Text :> QueryParam "capital" Bool :> Get '[JSON] User
+
+-- Datatypes
+
+data User = User
+  { name :: Text,
+    age :: Int,
+    email :: Text,
+    registration_date :: UTCTime
+  } 
+  deriving (Generic, Show, Typeable)
 
 -- type CodeProblemAPI = "code-problems"  :>
 
--- data SortBy = Age | Name
+instance FromJSON User
+instance ToJSON User
 
--- data User = User
---   { name :: String,
---     age :: Int,
---     email :: String,
---     registration_date :: UTCTime
---   }
+-- Server-side handlers
+userApiHandlers :: Server UserAPI
+userApiHandlers = users :<|> helloUser
+  where 
+    randomDate = UTCTime (fromGregorian 2018 10 27) (secondsToDiffTime 0)
+
+    users = return [ User "John Doe" 35 "some@gmail.com" randomDate ]
+
+    helloUser name Nothing      = helloUser name (Just False)
+    helloUser name (Just False) = return $ User name 35 "some@gmail.com" randomDate
+    helloUser name (Just True)  = return $ User (toUpper name) 35 "some@gmail.com" randomDate
--- server/app/Main.hs
+++ server/app/Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE RecordWildCards   #-}
+{-# LANGUAGE TypeOperators     #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PolyKinds         #-}
 {-# LANGUAGE TypeFamilies      #-}
@@ -28,53 +29,46 @@ import Data.Typeable (Typeable)
 import Servant
 import Servant.Server.Generic ()
 import Servant.API.Generic ((:-))
-import Servant.Swagger
 
-
-
-
+import Api.CodeProblem
 
 -- * Example
 
--- | A greet message data type
-newtype Greet = Greet { _msg :: Text }
-  deriving (Generic, Show, Typeable)
+-- -- | A greet message data type
+-- newtype Greet = Greet { _msg :: Text }
+--   deriving (Generic, Show, Typeable)
 
-instance FromJSON Greet
-instance ToJSON Greet
+-- instance FromJSON Greet
+-- instance ToJSON Greet
+
+-- instance ToSchema Greet where
+--   declareNamedSchema proxy = genericDeclareNamedSchema defaultSchemaOptions proxy
+--     & mapped.schema.description ?~ "This is some real Greet right here"
+--     & mapped.schema.example ?~ toJSON (Greet "Example greet msg for John Doe")
 
 
-instance ToSchema Greet where
-  declareNamedSchema proxy = genericDeclareNamedSchema defaultSchemaOptions proxy
-    & mapped.schema.description ?~ "This is some real Greet right here"
-    & mapped.schema.example ?~ toJSON (Greet "Example greet msg for John Doe")
-
-
+-- -- | API for serving @swagger.json@.
+-- type SwaggerAPI =  "swagger.json" :> Get '[JSON] Swagger
 -- API specification
-type TestApi =
-      -- | API for serving @swagger.json@.
-      "swagger.json" :> Get '[JSON] Swagger
+type API = UserAPI -- :<|>
 
-       -- GET /hello/:name?capital={true, false}  returns a Greet as JSON
-  :<|> "hello" :> Capture "name" Text :> QueryParam "capital" Bool :> Get '[JSON] Greet
+-- type TestApi =
+--   SwaggerAPI
+--        -- GET /hello/:name?capital={true, false}  returns a Greet as JSON
+--   :<|> "hello" :> Capture "name" Text :> QueryParam "capital" Bool :> Get '[JSON] Greet
+--        -- POST /greet with a Greet as JSON in the request body,
+--        --             returns a Greet as JSON
+--   :<|> "greet" :> ReqBody '[JSON] Greet :> Post '[JSON] Greet
+--        -- DELETE /greet/:greetid
+--   :<|> "greet" :> Capture "greetid" Text :> Delete '[JSON] NoContent
+--   :<|> NamedRoutes OtherRoutes
 
-       -- POST /greet with a Greet as JSON in the request body,
-       --             returns a Greet as JSON
-  :<|> "greet" :> ReqBody '[JSON] Greet :> Post '[JSON] Greet
+-- data OtherRoutes mode = OtherRoutes
+--   { version2 :: mode :- Get '[JSON] Int
+--   , bye :: mode :- "bye" :> Capture "name" Text :> Get '[JSON] Text
+--   }
+--   deriving Generic
 
-       -- DELETE /greet/:greetid
-  :<|> "greet" :> Capture "greetid" Text :> Delete '[JSON] NoContent
-
-  :<|> NamedRoutes OtherRoutes
-
-data OtherRoutes mode = OtherRoutes
-  { version2 :: mode :- Get '[JSON] Int
-  , bye :: mode :- "bye" :> Capture "name" Text :> Get '[JSON] Text
-  }
-  deriving Generic
-
-testApi :: Proxy TestApi
-testApi = Proxy
 
 -- Server-side handlers.
 --
@@ -82,48 +76,41 @@ testApi = Proxy
 -- that represents the API, are glued together using :<|>.
 --
 -- Each handler runs in the 'Handler' monad.
-server :: Server TestApi
-server = todoSwagger :<|> helloH :<|> postGreetH :<|> deleteGreetH :<|> otherRoutes
-  where otherRoutes = OtherRoutes {..}
+-- server :: Server TestApi
+-- server = return swagger :<|> helloH :<|> postGreetH :<|> deleteGreetH :<|> otherRoutes
+--   where otherRoutes = OtherRoutes {..}
 
-        bye name = pure $ "Bye, " <> name <> " !"
-        version = pure 42
+--         bye name = pure $ "Bye, " <> name <> " !"
+--         version = pure 42
 
-        helloH name Nothing = helloH name (Just False)
-        helloH name (Just False) = return . Greet $ "Hello, " <> name
-        helloH name (Just True) = return . Greet . toUpper $ "Hello, " <> name
+--         helloH name Nothing = helloH name (Just False)
+--         helloH name (Just False) = return . Greet $ "Hello, " <> name
+--         helloH name (Just True) = return . Greet . toUpper $ "Hello, " <> name
 
-        postGreetH greet = return greet
+--         postGreetH greet = return greet
 
-        deleteGreetH _ = return NoContent
+--         deleteGreetH _ = return NoContent
 
--- | Swagger spec for Todo API.
-todoSwagger :: Swagger
-todoSwagger = toSwagger testApi
-  & info.title   .~ "Todo API"
-  & info.version .~ "1.0"
-  & info.description ?~ "This is an API that tests swagger integration"
-  & info.license ?~ ("MIT" & url ?~ URL "http://mit.com")
+-- testApi :: Proxy TestApi
+-- testApi = Proxy
 
--- -- Turn the server into a WAI app. 'serve' is provided by servant,
--- -- more precisely by the Servant.Server module.
--- test :: Application
--- test = serve testApi server
-
--- -- Run the server.
--- --
--- -- 'run' comes from Network.Wai.Handler.Warp
--- runTestServer :: Port -> IO ()
--- runTestServer port = run port test
+-- -- | Swagger spec for Todo API.
+-- swagger :: Swagger
+-- swagger = toSwagger testApi
+--   & info.title   .~ "Todo API"
+--   & info.version .~ "1.0"
+--   & info.description ?~ "This is an API that tests swagger integration"
+--   & info.license ?~ ("MIT" & url ?~ URL "http://mit.com")
 
 
--- | Output generated @swagger.json@ file for the @'TodoAPI'@.
-writeSwaggerJSON :: IO ()
-writeSwaggerJSON = BL8.writeFile "example/swagger.json" (encodePretty todoSwagger)
+-- -- | Output generated @swagger.json@ file for the @'TodoAPI'@.
+-- writeSwaggerJSON :: IO ()
+-- writeSwaggerJSON = BL8.writeFile "server/swagger.json" (encodePretty swagger)
+
 
 -- Put this all to work!
 main :: IO ()
 main = do
   putStrLn "Running server on 8001"
   -- runTestServer 8001
-  run 8001 $ serve(Proxy :: Proxy TestApi) server
+  run 8001 $ serve(Proxy :: Proxy API) userApiHandlers
