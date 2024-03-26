03fab95785aaefc9047c7e4a472444d951b26af8
ChromaChroma <jona.beer@gmail.com>
2024-03-26 18:04:38 +0100

    Add Mockup of command line communication

--- server/app/Main.hs
+++ server/app/Main.hs
@@ -9,24 +9,78 @@ module Main where
 -- | Dependency imports
 import Control.Concurrent       (forkIO)
 import Control.Monad            (forever, guard)
-import Control.Monad.Catch      (MonadThrow, try)
+import Control.Monad.Catch      (MonadThrow, try, SomeException(..))
 import Control.Monad.Except     (ExceptT(..))
 import Control.Monad.Identity   (IdentityT (..))
 import Control.Monad.IO.Class   (MonadIO)
+import Data.Either              (Either(..))
+import Data.Text                (Text)
 import Network.Wai.Handler.Warp (run)
 import Servant.Server.Generic   (genericServeTWithContext)
 import Servant                  (Context(..), Handler(..))
+import System.Process
+import System.Exit
 -- | Project imports
 import Security.API             (api)
 import Security.App             (appToHandler)
 import Security.Auth            (authHandler, generateKey)
 import Security.Claims          (AccessClaims, RefreshClaims, accessSettings, refreshSettings)
-
-import System.Process
-import System.Exit
+import Dummy                    (dummyUUID)
 
 port :: Int
 port = 8080
+
+-- -- | Runs shell command and expects succesful termination
+-- runCmd :: FilePath -> [String] -> String -> IO () 
+-- runCmd cmd args inp = do 
+--   (exitCode, output, err) <- readProcessWithExitCode cmd args inp
+--   case exitCode of 
+--     ExitFailure _ -> putStrLn $ "Command `" ++ cmd ++ " " ++ foldr (\ x acc -> acc ++ " " ++ x) "" args ++ inp ++ "` failed. " ++ err
+--     ExitSuccess   -> pure ()
+--   -- guard (exitCode == ExitSuccess)
+
+-- runn ::FilePath -> [String] -> String -> IO ()
+-- runn cmd args inp = do
+--     res <- try $ readProcessWithExitCode cmd args inp
+--     case res of
+--         Left (SomeException _)        -> putStrLn "SoemExep. Ignoring..."
+--         Right (exitCode, output, err) -> case exitCode of 
+--                                           ExitFailure _ -> putStrLn $ "Command `" ++ cmd ++ " " ++ foldr (\ x acc -> acc ++ " " ++ x) "" args ++ inp ++ "` failed. " ++ err
+--                                           ExitSuccess   -> pure ()
+
+runWait :: String -> IO ExitCode
+runWait cmnd = runCommand cmnd >>= waitForProcess  
+  
+
+compileFile :: Text -> IO ()
+compileFile code = do 
+  -- now <- liftIO getCurrentTime
+  -- | Temp dir of user
+  let tempDir = "temp/" ++ show dummyUUID 
+ 
+  -- | Clean temp dir
+  runWait ("rm -r " ++ tempDir)
+  
+  -- | Create temp dir
+  runWait ("mkdir -p " ++ tempDir)
+  -- (exitCode, output, err) <- readProcessWithExitCode "mkdir" [] tempDir
+  -- guard (exitCode == ExitSuccess)
+
+  -- -- | Create file to be compiled
+  runWait ("echo " ++ show code ++ " >> " ++ tempDir ++ "/Main.hs")
+
+  -- | Compile file to same dir/dist (thus check validity of code)
+  
+  -- | run file as test/on test cases
+
+  -- | Build up result of code run
+
+  -- | Remove temp dir
+  -- runWait ("rm -r " ++ tempDir)
+
+  -- | Return result of run
+  putStrLn "Done Compiling"
+
 
 ghcCheck :: IO Bool
 ghcCheck = do
